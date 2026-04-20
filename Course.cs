5fce1397a598e9d8a79324a39947b1a1b8a180ad
ST10440871 <161321248+Atiyha@users.noreply.github.com>
2026-04-20 22:56:46 +0200

    Ice 1

---
+++ Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_1
+{
+    internal class Course
+    {
+        private int _grade;
+
+        public int Grade
+        {
+            get { return _grade; }
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException(nameof(value), "Grade must be between 0 and 100.");
+                _grade = value;
+            }
+        }
+
+        public bool IsPass => _grade >= 50;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+
+            Console.Write("Enter a grade: ");
+            int input = int.Parse(Console.ReadLine());
+
+            course.Grade = input;
+
+            if (course.IsPass)
+                Console.WriteLine($"Grade: {course.Grade} -  You Pass! :)");
+            else
+                Console.WriteLine($"Grade: {course.Grade} - You Fail! :(");
+        }
+    }
+}
